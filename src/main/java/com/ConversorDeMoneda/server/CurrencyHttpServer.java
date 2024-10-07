c7fb044965a91ff20a1f1271b3d223ec9e93b601
Alejandro Santis <asantiscastro@gmail.com>
2024-10-06 20:47:00 -0500

    Agregando iconos a la interfaz

--- src/main/java/com/ConversorDeMoneda/server/CurrencyHttpServer.java
+++ src/main/java/com/ConversorDeMoneda/server/CurrencyHttpServer.java
@@ -18,6 +18,7 @@ public class CurrencyHttpServer {
         server.createContext("/", exchange -> {
             String response = "<html><head><title>Currency Converter</title>" +
                     "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css\" rel=\"stylesheet\">" +
+                    "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css\">"+
                     "</head><body>" + generateHtmlForm() + "</body></html>";
             byte[] responseBytes = response.getBytes();
             exchange.sendResponseHeaders(200, responseBytes.length);
@@ -58,11 +59,16 @@ public class CurrencyHttpServer {
                         "</head><body>" +
                         "<div class=\"container mt-5\">" +
                         "<div class=\"card\">" +
-                        "<div class=\"card-header\">Resultado de la Conversion</div>" +
+                        "<div class=\"card-header\">" +
+                        "<i class=\"bi bi-currency-dollar\"></i>" +
+                        "Resultado de la Conversion" +
+                        "</div>" +
                         "<div class=\"card-body\">" +
                         "<h5 class=\"card-title\">Monto Convertido</h5>" +
                         "<p class=\"card-text\">" + amount + " " + currencyFrom + " equivale a " + result + " " + currencyTo + "</p>" +
-                        "<a href=\"/\" class=\"btn btn-primary\">Volver</a>" +
+                        "<a href=\"/\" class=\"btn btn-primary\">" +
+                        "<i class=\"bi bi-skip-backward\"></i>" +
+                        "Volver</a>" +
                         "</div></div></div></body></html>";
                 byte[] responseBytes = response.getBytes();
                 exchange.sendResponseHeaders(200, responseBytes.length);
@@ -84,12 +90,14 @@ public class CurrencyHttpServer {
     private static String generateHtmlForm() {
         return "<div class=\"container mt-5\">" +
                 "<div class=\"card\">" +
-                "<div class=\"card-header\">Convertidor de Monedas</div>" +
+                "<div class=\"card-header\">" +
+                "<i class=\"bi bi-currency-exchange\"></i>" +
+                " Convertidor de Monedas</div>" +
                 "<div class=\"card-body\">" +
                 "<form action=\"/convert\" method=\"POST\">" +
-                "<div class=\"mb-3\">" +
-                "<label for=\"amount\" class=\"form-label\">Monto</label>" +
-                "<input type=\"number\" class=\"form-control\" id=\"amount\" name=\"amount\" required>" +
+                "<div class=\"input-group mb-3\">" +
+                "<span class=\"input-group-text\" id=\"amount\">$</span>" +
+                "<input type=\"number\" class=\"form-control\" placeholder=\"Monto\" id=\"amount\" name=\"amount\" required>" +
                 "</div>" +
                 "<div class=\"mb-3\">" +
                 "<label for=\"currencyFrom\" class=\"form-label\">Desde</label>" +
@@ -109,7 +117,9 @@ public class CurrencyHttpServer {
                 "<option value=\"COP\">Peso Colombiano (COP)</option>" +
                 "</select>" +
                 "</div>" +
-                "<button type=\"submit\" class=\"btn btn-primary\">Convertir</button>" +
+                "<button type=\"submit\" class=\"btn btn-primary\">" +
+                "<i class=\"bi bi-caret-right\"></i>" +
+                "Convertir</button>" +
                 "</form></div></div></div>";
     }
 
